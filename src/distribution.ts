5a7d032f40c526298825d2827e879df9242c14ce
d4rk <ciml@protonmail.com>
2023-10-13 15:15:08 +0300

    upd 0.4.0
    - remove Axios, add func from "@cosmjs/tendermint-rpc"

--- src/distribution.ts
+++ src/distribution.ts
@@ -1,10 +1,13 @@
-import axios from "axios";
+import { QueryClient, setupDistributionExtension } from "@cosmjs/stargate";
+import { Tendermint34Client } from "@cosmjs/tendermint-rpc";
 import { MsgWithdrawDelegatorReward } from "cosmjs-types/cosmos/distribution/v1beta1/tx.js";
 import { config } from "../config.js";
 
 export const getDelegatorValidators = async(address: string) => {
-    const res = await axios.get(`${config.restApi}/cosmos/distribution/v1beta1/delegators/${address}/validators`);
-    return res.data.validators;
+    const tmClient = await Tendermint34Client.connect(config.rpc);
+    const client = QueryClient.withExtensions(tmClient, setupDistributionExtension);
+    const res = await client.distribution.delegatorValidators(address);
+    return res.validators;
 }
 
 export const dataClaimRewards = async(delegator: string, validator: string) => {
