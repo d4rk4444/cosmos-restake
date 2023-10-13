9d33ca2be0a99bf7edd738edb25c5904aeea95f2
d4rk <ciml@protonmail.com>
2023-10-13 15:18:35 +0300

    fix 0.4.0

--- config.ts
+++ config.ts
@@ -1,6 +1,5 @@
 export const config = { //Example Config
     rpc: 'https://umee-rpc.w3coins.io',
-    restApi: 'https://umee-api.w3coins.io',
     coin: 'uumee',
     gasPrice: '0.1',
     stakingValidator: 'umeevaloper1tsd7h4erlx9wajg353dwjc56lrvlcnmeghnmk0',
